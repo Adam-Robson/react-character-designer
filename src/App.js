30bca5d833488ede912393b639dfd91a082e9b9e
Adam Robson <adamray312@gmail.com>
2022-09-23 23:48:39 -0700

    add animated background

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
   return (
     <div className="App">
       <Main />
+      <div className="block n1"></div>
+      <div className="block n2"></div>
+      <div className="block n3"></div>
+      <div className="block n4"></div>
+      {/* <img className="tvhead" src={process.env.PUBLIC_URL + `/images/tvhead.png` } alt="" /> */}
+
     </div>
   );
 }
