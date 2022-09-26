c80dcccf76f8a02098267ce2b1f1ce03899cda92
Adam Robson <adamray312@gmail.com>
2022-09-26 11:43:06 -0700

    add css styling: not finished

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 export default function Header() {
   return (
     <section className="headerContainer">
-      <div className="header">character<br/>building</div>
+      <div className="header">character<br />building.</div>
     </section>
   );
 }
--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -3,9 +3,9 @@ import './Stats.css';
 export default function Stats({ headCount, torsoCount, legsCount }) {
   return (
     <div className="statsContainer">
-      <p>You have changed the head { headCount } times.</p>
-      <p>You have changed the torso { torsoCount } times.</p>
-      <p>You have changed the legs {legsCount} times.</p>
+      <p className="headContainer">You have changed the head { headCount } times.</p>
+      <p className="torsoContainer">You have changed the torso { torsoCount } times.</p>
+      <p className="legsContainer">You have changed the legs {legsCount} times.</p>
     </div>
   );
 }
